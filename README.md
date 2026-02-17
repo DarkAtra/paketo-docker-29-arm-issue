d1548d800df231d1f09d28b3afe9e6d694f6f273
Tommy Schmidt <tommy.schmidt@idealo.de>
2026-02-17 17:04:45 +0100

    chore: add reproducer

---
+++ README.md
@@ -0,0 +1,4 @@
+# `paketo-docker-29-arm-issue`
+
+Reproducer for the issue described
+in: https://github.com/paketo-buildpacks/builder-noble-java-tiny/issues/191#issuecomment-3913492921.
---
+++ src/main/java/de/darkatra/paketodocker29armissue/PaketoDocker29ArmIssueApplication.java
@@ -0,0 +1,12 @@
+package de.darkatra.paketodocker29armissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaketoDocker29ArmIssueApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(PaketoDocker29ArmIssueApplication.class, args);
+    }
+}
